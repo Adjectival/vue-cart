2b74b48de1fef3cbe8529fd744a1127398bb0ad7
Adjectival <alexd.jacks@gmail.com>
2018-03-06 16:25:55 -0800

    :sparkles: adds examples of common UX directives

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,34 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <h2>{{ msg2 }}</h2>
-    <h2 class="bg-info">Essential Links</h2>
+    <h2 class="mauvey">Data Binding & Directives Example</h2>
+    <div class="card p-1 m-3">
+      <h3>
+        <i class="fas fa-magic"></i>
+        Wizards
+      </h3>
+      <!-- iterate over a list -->
+      <ul>
+        <li v-for="wizard in wizards">
+          {{ wizard.first }} {{ wizard.last }}
+        </li>
+        <li>
+          <span v-text="input_val"></span>
+        </li>
+      </ul>
+        <!-- Two-way data binding -->
+        <div>
+          <span>Add a name: </span>
+          <input type="text" v-model="input_val">
+        </div>
+        <!-- counter w/ interpolation -->
+        <div>
+          <button class="btn btn-secondary my-2" v-on:click="counter++">
+            Button clicked {{counter}} times
+          </button>
+        </div>
+    </div>
+    <h2 class="bg-info">Essential Vue.js Links</h2>
     <ul class="card p-1 m-3">
       <li><i class="fas fa-book"></i>
         <a
@@ -46,7 +73,7 @@
         </a>
       </li>
     </ul>
-    <h2 class="bg-info">Ecosystem</h2>
+    <h2 class="bg-info">Vue.js Ecosystem</h2>
     <ul class="card p-1 m-3">
       <li><i class="fas fa-cog"></i>
         <a
@@ -91,7 +118,17 @@ export default {
     return {
       msg: 'Enjoy Vue.js',
       msg2: 'Bootstrap4 and FontAwesome5 are gtg!',
-
+      //<!-- iterate over a list -->
+      wizards: [
+        { first: 'Ginny', last: 'Weasley' },
+        { first: 'Severus', last: 'Snape' },
+        { first: 'Hermione', last: 'Granger' },
+        { first: 'Albus', last: 'Dumbledore' },
+      ],
+      //<!-- Two-way data binding -->
+      input_val: '',
+      //<!-- counter w/ interpolation -->
+      counter: 0,
     };
   },
 };
@@ -101,6 +138,9 @@ export default {
 <style scoped>
 h1, h2 {
   font-weight: normal;
+}
+.mauvey {
+  background-color: #ac54a1;
 }
 ul {
   list-style-type: none;
