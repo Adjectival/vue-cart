18e08e87be1e652be4636b1c0bb2059fcb8f067b
Adjectival <alexd.jacks@gmail.com>
2018-03-18 18:20:22 -0700

    :construction: adds initial store data

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,12 +1,15 @@
 <template>
     <nav class="navbar navbar-light bg-light" id="navbar">
-  <a class="navbar-brand" href="#!">Navbar</a>
-</nav>
+        <a class="navbar-brand" href="#!">Navbar</a>
+        <router-link class="nav-link active" href="#!">Active</router-link>
+        <router-link class="nav-link" href="#!">Link</router-link>
+        <router-link class="nav-link disabled" href="#!">Disabled</router-link>
+    </nav>
 </template>
 
 <script>
 export default {
-  name: "",
+  name: "navbar",
   data: () => ({
 
   })
--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,41 @@ Vue.use(Vuex)
 
 const debug = process.env.NODE_ENV !== 'production'
 
-// mutations
+// app starts with these =aka= initial state
+const state = {
+    added: [],
+    all: [
+        {
+            id: 'mo6dy4uyrfzp',
+            name: 'Ether',
+            description: 'Restores 50 Magic Points. Tastes like spearmint.',
+            price: 500
+        },
+        {
+            id: '3x9xk0xid7le',
+            name: 'Glass Helmet',
+            description: 'Purely decorative headgear, modeled on actual parade armor. Intended for display indoors.',
+            price: 22000
+        },
+        {
+            id: '13rumtpz0y0y',
+            name: 'Steel Bucket',
+            description: 'Very useful, can carry liquids or trap small creatures.',
+            price: 800
+        },
+        {
+            id: 'jwt9yhkqe1q0',
+            name: 'Junior Archery Set',
+            description: 'Designed for inexperienced bowpersons, requires only 10 eeks of draw strength. Comes with rubber safety arrows',
+            price: 8000
+        },
+    ]
+}
+
+
+
+
+// state mutations allowed in store
 const mutations = {
 
     [types.ADD_TO_CART] (state, { id }) {
@@ -29,5 +63,7 @@ const mutations = {
 export default new Vuex.Store({
     state,
     strict: debug,
-    mutations
+    mutations,
+    getters,
+    setters
 })
