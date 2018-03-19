7b978a9a80835f27f8b4c53249856e4fdbcd4c26
Adjectival <alexd.jacks@gmail.com>
2018-03-18 19:23:30 -0700

    :sparkles: builds product view for all store data, ties to store

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,15 +1,49 @@
 <template>
-  <div id="products">
-
+  <div class="container-fluid text-left">
+      <h1>Our Store, All Products</h1>
+      <h3>Featuring {{ length }} products</h3>
+      <table class="table table-striped">
+          <thead>
+              <tr>
+                  <th>Name</th>
+                  <th>Description</th>
+                  <th>Price</th>
+                  <th></th>
+              </tr>
+          </thead>
+          <tbody>
+              <tr
+              v-for="product in products"
+              track-by="id"
+              >
+                <td>{{product.name}}</td>
+                <td>{{product.description}}</td>
+                <td>{{product.price}}</td>
+                <td>
+                    <button
+                    @click="addToCart(product)"
+                    class="btn btn-lg btn-info"
+                    >
+                    Add To Cart
+                    </button>
+                </td>
+              </tr>
+          </tbody>
+      </table>
   </div>
 </template>
 
 <script>
+import { mapGetters, mapActions } from 'vuex'
 export default {
-  name: "products",
-  data: () => ({
-
-  })
+  name: "app",
+  computed: mapGetters({
+      products: 'allProducts',
+      length: 'numberOfProducts'
+  }),
+  methods: mapActions([
+      'addToCart'
+  ])
 }
 </script>
 
